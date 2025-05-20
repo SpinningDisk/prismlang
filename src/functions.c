0f9acbd76269d1cd59b1e5e5956f48ac2280f965
SpinningDisk <150556546+SpinningDisk@users.noreply.github.com>
2025-05-20 17:05:08 +0000

    hans

--- src/functions.c
+++ src/functions.c
@@ -3,9 +3,15 @@
 #include <malloc.h>
 
 // custom types
+//      big int
 typedef struct { int64_t parts[2]; }int128_t;
 typedef struct { int64_t parts[4]; }int256_t;
 typedef struct { int64_t parts[8]; }int512_t;
 typedef struct { uint64_t parts[2]; }uint128_t;
 typedef struct { uint64_t parts[4]; }uint256_t;
 typedef struct { uint64_t parts[8]; }uint512_t;
+
+//      float
+typedef struct __attribute__((packed), alligin()){
+    int
+}
